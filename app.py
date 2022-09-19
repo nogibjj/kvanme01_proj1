10793b64fd8bcc1f97cec08b5b0ce53ff9d89c16
kvanme01 <112578194+kvanme01@users.noreply.github.com>
2022-09-19 06:51:21 +0000

    app

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-import json
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 app = Flask(__name__)
 @app.route("/")
+
 def home():
     return "Hello, Flask!"
