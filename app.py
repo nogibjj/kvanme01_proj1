a2c97d512e0dd8f4fd38ec1478f2a899db8f96a3
kvanme01 <112578194+kvanme01@users.noreply.github.com>
2022-09-19 23:46:19 +0000

    using click

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+import click
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+@click.command()
+def hello():
+    click.echo("Hello, there!")
+
+if __name__ == "__main__":
+    hello()
+    app.run()
