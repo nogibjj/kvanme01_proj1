eb526f4ead08387341167b84731c57ee1f7d6a37
kvanme01 <112578194+kvanme01@users.noreply.github.com>
2022-09-20 00:33:32 +0000

    new notebook to fix venv problem

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 from flask import Flask
-import pandas as pd
-
-# read in csv data
-df = pd.read_csv("wnbadraft.csv")
-
-# create a random starting lineup
-lineup = df.sample(n=5)
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello World"
+    return "Hello, World!"
