3b6c1dd4f779b95baff6e1e0173d7ef72b04b16e
kvanme01 <112578194+kvanme01@users.noreply.github.com>
2022-09-20 00:14:52 +0000

    flask app

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask
-import click
+import pandas as pd
+
+# read in csv data
+df = pd.read_csv("wnbadraft.csv")
+
+# create a random starting lineup
+lineup = df.sample(n=5)
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "Hello, Flask!"
-
-@click.command()
-def hello():
-    click.echo("Hello, there!")
-
-if __name__ == "__main__":
-    hello()
-    app.run()
+    return "Hello World"
